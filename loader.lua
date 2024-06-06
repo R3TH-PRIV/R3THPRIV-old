36064459339875ee0c8e06e596cc7eed3aa47a30
R3TH PRIV <119117781+R3TH-PRIV@users.noreply.github.com>
2024-06-06 20:15:44 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,6 @@
 --[[
     R3TH PRIV LOADER SOURCE
 
-    It should be noted that this script is rather messy and not recommended for learning off because it is many months old and my first script hub ever.
-    V2 is being developed, and now that I've had six months of experience with lua, I can create a much better script hub.
     .gg/pethicial
 
     Credits:
@@ -10,112 +8,78 @@
 ]]
 
 --------------------------------------------------------------------------------------R3THPRIV----------------------------------------------------------------------------------------
-print("[ R3TH PRIV ]: R3TH PRIV Loader executed.")
-local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+repeat wait() until game:IsLoaded()
 
-function sendnotification(notfiicationmessage)
-    if R3THDEVICE == "Mobile" then
-        game:GetService("StarterGui"):SetCore("SendNotification", {
-            Title = "R3TH PRIV";
-            Text = notfiicationmessage;
-            Duration = 5;
-        })
-    else
-        Notification:Notify(
-            {Title = "R3TH PRIV", Description = notfiicationmessage},
-            {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "default"}
-        )
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+local StarterGui = game:GetService("StarterGui")
+local UIS = game:GetService("UserInputService")
+local Touchscreen = UIS.TouchEnabled
+
+local games = {
+    [142823291] = 'Murder%20Mystery%202',
+    [335132309] = 'Murder%20Mystery%202',
+    [636649648] = 'Murder%20Mystery%202',
+    [7863262550] = 'FOBLOX',
+    [5244411056] = 'Bloxy%20Bingo',
+    [8908228901] = 'SharkBite%202',
+    [6847090259] = 'Bulked%20Up',
+    [12151453277] = 'Abyss%20World',
+    [2693739238] = 'THIEF%20LIFE%20Simulator',
+    [3891618314] = 'Harbor%20Havoc',
+    [16281300371] = 'Blade%20Ball',
+    [13772394625] = 'Blade%20Ball',
+    [14732610803] = 'Blade%20Ball',
+    [4889315193] = 'Total%20Roblox%20Drama',
+    [4939362930] = 'Total%20Roblox%20Drama',
+    [4939362610] = 'Total%20Roblox%20Drama',
+    [7172152072] = 'Total%20Roblox%20Drama',
+    [1962086868] = 'Tower%20of%20Hell',
+    [3582763398] = 'Tower%20of%20Hell',
+    [5253186791] = 'Tower%20of%20Hell',
+}
+
+function sendnotification(message, type)
+    if type == false or type == nil then
+        print("[ R3TH PRIV ]: " .. message)
+    end
+    if type == true or type == nil then
+        if R3THDEVICE == "Mobile" then
+            StarterGui:SetCore("SendNotification", {
+                Title = "R3TH PRIV";
+                Text = message;
+                Duration = 7;
+            })
+        else
+            Notification:Notify(
+                {Title = "R3TH PRIV", Description = message},
+                {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 7, Type = "default"}
+            )
+        end
     end
 end
 
-if r3thexecuted then
-    print("[ R3TH PRIV ]: Script already executed, if your having any problems join discord.gg/pethicial for support.")
-    sendnotification("Script already executed, if your having any problems join discord.gg/pethicial for support.")
+if getgenv().r3thexecuted then
+    sendnotification("Script already executed, if you're having any problems join discord.gg/pethicial for support.", nil)
     return
 end
-
 getgenv().r3thexecuted = true
 
-local UIS = game:GetService("UserInputService")
-local PC, PCwithTouch, Touchscreen = (UIS.MouseEnabled and UIS.KeyboardEnabled), (UIS.MouseEnabled and UIS.KeyboardEnabled and UIS.TouchEnabled), UIS.TouchEnabled
-if Touchscreen == true then
-    getgenv().R3THDEVICE = "Mobile"
+--------------------------------------------------------------------------------------LOADER----------------------------------------------------------------------------------------
+getgenv().R3TH_Device = Touchscreen and "Mobile" or "PC"
+sendnotification(R3TH_Device .. " detected.", false)
+
+getgenv().R3TH_Hook = (type(hookmetamethod) == "function" and type(getnamecallmethod) == "function") and "Supported" or "Unsupported"
+sendnotification("Hook method is " .. R3TH_Hook .. ".", false)
+
+getgenv().R3TH_Drawing = (type(Drawing.new) == "function") and "Supported" or "Unsupported"
+sendnotification("Drawing.new is " .. R3TH_Drawing .. ".", false)
+
+sendnotification("Script loading, this may take a while depending on your device.", nil)
+
+if games[game.PlaceId] then
+    sendnotification("Game Supported!", false)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/' .. games[game.PlaceId] .. '.lua'))()
 else
-    getgenv().R3THDEVICE = "PC"
-end
-
-if type(hookmetamethod) and type(getnamecallmethod) == "function" then
-    getgenv().R3THEXECUTOR = "Supported"
-else
-    getgenv().R3THEXECUTOR = "Unsupported"
-end
-
-if R3THEXECUTOR == "Unsupported" then
-    print("[ R3TH PRIV ]: Unsupported executor detected, some scripts may not work.")
-    sendnotification("Unsupported executor detected, some scripts may not work.")
-end
-
-print("[ R3TH PRIV ]: Script loading, this may take awhile depending on your device.")
-sendnotification("Script loading, this may take awhile depending on your device.")
-
-if game.PlaceId == (142823291 or 335132309 or 636649648) then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Murder%20Mystery%202.lua'))()
-elseif game.PlaceId == 7863262550 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/FOBLOX.lua'))()
-elseif game.PlaceId == 5244411056 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Bloxy%20Bingo.lua'))()
-elseif game.PlaceId == 8908228901 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/SharkBite%202.lua'))()
-elseif game.PlaceId == 6847090259 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Bulked%20Up.lua'))()
-elseif game.PlaceId == 12151453277 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Abyss%20World.lua'))()
-elseif game.PlaceId == 2693739238 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/THIEF%20LIFE%20Simulator.lua'))()
-elseif game.PlaceId == 3891618314 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Harbor%20Havoc.lua'))()
-elseif game.PlaceId == 16281300371 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Blade%20Ball.lua'))()
-elseif game.PlaceId == 13772394625 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Blade%20Ball.lua'))()
-elseif game.PlaceId == 14732610803 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Blade%20Ball.lua'))()
-elseif game.PlaceId == 13772394625 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Blade%20Ball.lua'))()
-elseif game.PlaceId == 4889315193 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
-elseif game.PlaceId == 4939362930 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
-elseif game.PlaceId == 4939362610 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
-elseif game.PlaceId == 7172152072 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Total%20Roblox%20Drama.lua'))()
-elseif game.PlaceId == 1962086868 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Tower%20of%20Hell.lua'))()
-elseif game.PlaceId == 3582763398 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Tower%20of%20Hell.lua'))()
-elseif game.PlaceId == 5253186791 then
-    print("[ R3TH PRIV ]: Game Supported!")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Tower%20of%20Hell.lua'))()
-else
-    print("[ R3TH PRIV ]: Game not Supported.")
+    sendnotification("Game not Supported.", false)
     loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/Games/Universal.lua'))()
 end
