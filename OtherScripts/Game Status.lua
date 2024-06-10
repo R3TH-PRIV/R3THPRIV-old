ca582ba127a58b6167ac4840bbc1e8d909af7e4a
R3TH PRIV <119117781+R3TH-PRIV@users.noreply.github.com>
2024-06-10 16:43:44 +0800

    Update Game Status.lua

--- OtherScripts/Game Status.lua
+++ OtherScripts/Game Status.lua
@@ -1,6 +1,6 @@
 R3THPRIVV1_AbyssWorld = "Working"
 R3THPRIVV1_BladeBall = "Down"
-R3THPRIVV1_BloxyBingo = "Patched, will fix."
+R3THPRIVV1_BloxyBingo = "Working"
 R3THPRIVV1_BulkedUp = "Down"
 R3THPRIVV1_FOBLOX = "Down"
 R3THPRIVV1_HarborHavoc = "Down"
