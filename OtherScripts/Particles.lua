204dea244c9a2ecfaa2eef024af2e48e6594d5f5
R3TH PRIV <119117781+R3TH-PRIV@users.noreply.github.com>
2024-06-24 01:22:10 +0800

    Create Particles.lua

---
+++ OtherScripts/Particles.lua
@@ -0,0 +1,160 @@
+--[[
+    R3TH PRIV PARTICLES SOURCE
+
+    .gg/pethicial
+
+    Credits:
+        pethicial
+]]
+
+local Players = game:GetService("Players")
+
+function AddParticles(player)
+    local character = player.Character
+    repeat wait() until character
+    for i,v in pairs (character:GetChildren()) do
+        if v:IsA("Part") then
+            local particle = Instance.new("ParticleEmitter")
+            particle.Texture = "http://www.roblox.com/asset/?id=11839269455"
+            particle.Parent = v
+            particle.Rate = 5
+            particle.Size = NumberSequence.new(0.5)
+        end
+    end
+end
+
+local UserIds = {
+    4395539224,
+    4395558570,
+    4383123554,
+    4388471465,
+    4385548043,
+    4388464464,
+    4388460549,
+    4395547247,
+    4395542413,
+    4386584832,
+    4385549429,
+    4386587913,
+    4395553797,
+    4388455062,
+    4388456980,
+    4386593321,
+    4383124873,
+    4386596394,
+    4386581560,
+    5025025056,
+    4385546463,
+    4381780232,
+    4381789436,
+    4381783100,
+    4381785992,
+    4401330552,
+    4401332514,
+    4406464584,
+    4406459346,
+    4401356153,
+    4401336096,
+    4406479654,
+    4406461330,
+    4401344195,
+    4406468786,
+    4406466727,
+    4406475738,
+    4406454273,
+    4401346447,
+    4406477330,
+    4401341231,
+    4381791929,
+    4383120960,
+    4383116307,
+    4383117488,
+    5025041589,
+    5025465374,
+    5025046418,
+    4401337786,
+    4401325092,
+    5025037655,
+    5025459922,
+    4383114532,
+    4383122330,
+    5025099676,
+    5025367069,
+    5025401829,
+    5025406785,
+    4422118934,
+    5025252742,
+    5025385732,
+    5025221418,
+    5025452495,
+    5025185886,
+    5025231054,
+    5025163459,
+    5025348534,
+    5025329932,
+    5025105289,
+    5025171331,
+    5025395881,
+    5025196142,
+    5025245476,
+    5025213323,
+    5025272402,
+    5025028636,
+    5025096436,
+    5025322541,
+    5025202029,
+    5025209358,
+    403528605,
+    5025224034,
+    5025102483,
+    5025292258,
+    5025389882,
+    5025415474,
+    5025085767,
+    5025092149,
+    5025449393,
+    5025411594,
+    5025241342,
+    5025178293,
+    5025089141,
+    5025316909,
+    5025340430,
+    5025261429,
+    5025357248,
+    5025440102,
+    5025374820,
+    5025281125,
+    5025287866,
+    4406473673,
+    4401327886,
+}
+
+local function isInUserIdList(UserId)
+    for i, v in ipairs(UserIds) do
+        if UserId == v then
+            return true
+        end
+    end
+    return false
+end
+
+local function AddListener(player)
+    if player ~= Players.LocalPlayer then
+        player.CharacterAdded:Connect(function(character)
+            AddParticles(player)
+        end)
+        AddParticles(player)
+    end
+end
+
+for i,v in pairs(Players:GetPlayers()) do
+    if isInUserIdList(v.UserId) then
+        AddListener(v)
+    end
+end
+
+Players.PlayerAdded:Connect(function(player)
+    if isInUserIdList(player.UserId) then
+        AddListener(player)
+    end
+end)
