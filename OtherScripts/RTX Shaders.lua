7d6e1e0dfde601edd9710ddea498d719bdeb566c
R3TH PRIV <119117781+R3TH-PRIV@users.noreply.github.com>
2024-06-29 23:20:51 +0800

    Update RTX Shaders.lua

--- OtherScripts/RTX Shaders.lua
+++ OtherScripts/RTX Shaders.lua
@@ -1,2 +1,22 @@
--- huge L still using this link Jann Hub
-print("testing")
+-- Jann Hub You are a major disappointment for using this link still and skidding my shit right after I upload it. --
+game:GetService("CoreGui").Rayfield:Destroy()
+
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+local function sendnotification(message)
+    Notification:Notify(
+        {Title = "R3TH PRIV | .gg/pethicial", Description = message},
+        {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 10, Type = "default"}
+    )
+end
+
+sendnotification("WARNING JANN HUB RECORDS ALL OF YOUR INFORMATION AND IS DETECTED, PUTTING YOU AT RISK OF BEING BANNED.")
+wait(10)
+sendnotification("I am Pethicial, the owner of 'R3TH PRIV', and I am advising you to avoid Jann Hub since it is extremely dangerous.")
+wait(10)
+sendnotification("R3TH PRIV, in contrast to Jann Hub, is completely undetectable, keyless, and gives a lot more possibilities.")
+wait(10)
+sendnotification("I'll load R3TH PRIV for you right now and make sure to avoid risky scripts like Jann Hub in the future.")
+wait(10)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/loader.lua'))()
